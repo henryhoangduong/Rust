205b6396b06a1ca9f3e1e7a5d2c13761574cef3e
henryhoangduong <henryhoangduong@gmail.com>
2025-05-21 13:54:58 +0700

    webserver

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -13,8 +14,21 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK \r\n\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+
+        let response = format!("HTTP/1.1 200 OK \r\n\n {}", contents.len(), contents);
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{} \r\nContent-Length", contents.len(), contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
